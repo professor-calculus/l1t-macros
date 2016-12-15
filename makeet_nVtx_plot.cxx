9dd4038f9b3700921ee30d261c8bd2c01c65f234
professor-calculus <alextitterton@physics.org>
2016-12-15 17:05:16 +0000

    Different bins for et_dens

--- makeet_nVtx_plot.cxx
+++ makeet_nVtx_plot.cxx
@@ -51,7 +51,9 @@ void makeet_nVtx_plot(std::string run, double cut_et, int cut_ieta)
     
     std::vector<double> eta_values = {0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.087, 0.09, 0.1, 0.113, 0.129, 0.15, 0.178, 0.15, 0.35};
     
-    std::vector<double> etbins = {5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80};
+    std::vector<double> etbins = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60};
+    
+    std::vector<double> etbins_dens = {0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,250,260,270,280,290,300,310,320,330,340,350,360,370,380,390,400,410,420,430,440,450,460,470,480,490,500,510,520,530,540,550,560,570,580,590,600};
     
     
     std::vector<std::string> inDir;
@@ -77,7 +79,7 @@ void makeet_nVtx_plot(std::string run, double cut_et, int cut_ieta)
     et_ieta_plots.emplace_back(new TL1et_ieta_plot());
     et_ieta_plots[1]->SetX("et_density_vs_nVtx","ieta");
     et_ieta_plots[1]->SetXBins(puBins);
-    et_ieta_plots[1]->SetYBins(etbins);
+    et_ieta_plots[1]->SetYBins(etbins_dens);
     et_ieta_plots[1]->SetOutName(triggerName+"_et_density_vs_nVtx");
     
     std::vector<int> metbins = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,99999};
